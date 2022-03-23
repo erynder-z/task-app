096293b27fca27fc1f203fea7b670505435da9d7
eryner-z <stfn.bgr@gmail.com>
2022-03-23 21:54:09 +0100

    Render task from input field on button press

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-function App() {
-  return <div></div>;
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      task: '',
+    };
+  }
+
+  handleChange(e) {
+    this.setState({ input: e.target.value });
+  }
+
+  handleClick() {
+    this.setState({ task: this.state.input });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <label htmlFor="taskInput">Input Task</label>
+          <input
+            type="text"
+            name="taskInput"
+            /* onChange={this.handleChange.bind(this)} */
+            onChange={(e) => this.handleChange(e)}
+          />
+          <button onClick={() => this.handleClick()}>Submit</button>
+          <h1>{this.state.task}</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
